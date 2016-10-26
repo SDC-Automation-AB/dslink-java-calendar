08c86654aa93a95a4f2ce420f765cc45c4cf398b
Joel Trottier-Hebert <iamquebecois@gmail.com>
2016-10-25 21:35:13 -0400

    Some refactoring

--- src/test/java/org/dsa/iot/calendar/event/EventUtilsTest.java
+++ src/test/java/org/dsa/iot/calendar/event/EventUtilsTest.java
@@ -19,6 +19,7 @@ public class EventUtilsTest {
     private static final String TITLE = "jlkfdsjalk";
     private final Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
     private final Duration eventDuration = Duration.of(1, HOURS);
+    private final Instant now = Instant.now(clock);
     private Instant startOfEvent;
 
     @Before
@@ -49,7 +50,6 @@ public class EventUtilsTest {
 
     @Test
     public void when_only_one_event_and_its_over() {
-        Instant now = Instant.now(clock);
         Instant startOfEvent = now.minus(10, HOURS);
         Instant endOfEvent = now.minus(1, HOURS);
         TimeRange expectedResult = new TimeRange(now, now.plus(wantedDuration));
@@ -68,12 +68,11 @@ public class EventUtilsTest {
      */
     @Test
     public void after_two_contiguous_events_when_one_is_currently_active() {
-        Instant now = Instant.now(clock);
+        TimeRange expectedResult = new TimeRange(now.plus(2, HOURS), now.plus(5, HOURS));
         List<DSAEvent> events = Lists.newArrayList(
                 new DSAEvent(TITLE, now.minus(1, HOURS), now.plus(1, HOURS)),
                 new DSAEvent(TITLE, now.plus(1, HOURS), now.plus(2, HOURS))
         );
-        TimeRange expectedResult = new TimeRange(now.plus(2, HOURS), now.plus(5, HOURS));
 
         TimeRange result = EventUtils.findNextFreeTimeRange(events, Duration.of(3, HOURS));
 
@@ -85,14 +84,11 @@ public class EventUtilsTest {
      */
     @Test
     public void free_time_between_events() {
-        Instant now = Instant.now(clock);
-
+        TimeRange expectedResult = new TimeRange(now.plus(1, HOURS), now.plus(4, HOURS));
         List<DSAEvent> events = Lists.newArrayList(
                 new DSAEvent(TITLE, now.minus(1, HOURS), now.plus(1, HOURS)),
                 new DSAEvent(TITLE, now.plus(4, HOURS), now.plus(5, HOURS))
         );
-
-        TimeRange expectedResult = new TimeRange(now.plus(1, HOURS), now.plus(4, HOURS));
 
         TimeRange result = EventUtils.findNextFreeTimeRange(events, Duration.of(3, HOURS));
 
@@ -104,16 +100,13 @@ public class EventUtilsTest {
      */
     @Test
     public void first_free_time_between_events_not_big_enough() {
-        Instant now = Instant.now(clock);
-
+        TimeRange expectedResult = new TimeRange(now.plus(4, HOURS), now.plus(7, HOURS));
         List<DSAEvent> events = Lists.newArrayList(
                 new DSAEvent(TITLE, now.minus(1, HOURS), now.plus(1, HOURS)),
                 new DSAEvent(TITLE, now.plus(3, HOURS), now.plus(4, HOURS)),
                 new DSAEvent(TITLE, now.plus(7, HOURS), now.plus(8, HOURS))
 
         );
-
-        TimeRange expectedResult = new TimeRange(now.plus(4, HOURS), now.plus(7, HOURS));
 
         TimeRange result = EventUtils.findNextFreeTimeRange(events, Duration.of(3, HOURS));
 
